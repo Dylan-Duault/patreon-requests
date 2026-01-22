ee2dcf428421f60f0dc3323e6a8608177a9f35b8
Dylan Duault <contact@dylan-duault.com>
2026-01-22 20:41:11 +0100

    feat: add patreon subscribe URL to shared props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -57,6 +57,7 @@ class HandleInertiaRequests extends Middleware
                 'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
             ],
+            'patreonSubscribeUrl' => config('services.patreon.subscribe_url'),
         ];
     }
 }
