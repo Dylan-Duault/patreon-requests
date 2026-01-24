0abe0bef58818fafbe4dee28802e383a0e599ea8
Dylan Duault <contact@dylan-duault.com>
2026-01-24 23:48:29 +0100

    chore: branding

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -9,6 +9,6 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-black dark:text-white" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold">Requests Manager</span>
+        <span class="mb-0.5 truncate leading-tight font-semibold">The world with D</span>
     </div>
 </template>
